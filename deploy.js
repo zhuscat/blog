46f93223173e0e6244f9d94922897d8afd670547
zhuscat <zhudx6512@163.com>
2019-04-14 10:33:03 +0800

    feat: remove Coding.net deploy

--- deploy.js
+++ deploy.js
@@ -1,25 +1,17 @@
 const ghpages = require('gh-pages')
 
-ghpages.publish('public', {
-  branch: 'master',
-  repo: 'git@git.coding.net:zhuscat/zhuscat.git',
-}, (err) => {
-  if (err) {
-    console.error(err)
-    console.error('Deploying to Coding.net Failed')
-    return
+ghpages.publish(
+  'public',
+  {
+    branch: 'master',
+    repo: 'git@github.com:zhuscat/zhuscat.github.io.git',
+  },
+  err => {
+    if (err) {
+      console.error(err)
+      console.error('Deploying to GitHub Failed')
+      return
+    }
+    console.log('Deployed to GitHub')
   }
-  console.log('Deployed to Coding.net')
-})
-
-ghpages.publish('public', {
-  branch: 'master',
-  repo: 'git@github.com:zhuscat/zhuscat.github.io.git',
-}, (err) => {
-  if (err) {
-    console.error(err)
-    console.error('Deploying to GitHub Failed')
-    return
-  }
-  console.log('Deployed to GitHub')
-})
+)
